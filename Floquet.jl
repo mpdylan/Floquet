5b76dcc89ab68d4fae88aa6eb55be738c766c9ec
Dylan Murphy <dtaquinas@gmail.com>
2016-10-12 19:38:02 -0700

    Misc comments and stuff

--- Floquet.jl
+++ Floquet.jl
@@ -1,13 +1,19 @@
 module Floquet
 
-function floqdisc(f, λ, P)
+import ODE.ode23
+
+## Routines for the continuous setting
+
+function disccts(f, λ, P)
   function rhs(t, y)
     return [y[2], f(t)*y[1] - λ*y[1]]
   end
-  t1, y1 = ode45(rhs, [1.,0.], [0., P])
-  t2, y2 = ode45(rhs, [0.,1.], [0., P])
+  t1, y1 = ode23(rhs, [1.,0.], [0., P])
+  t2, y2 = ode23(rhs, [0.,1.], [0., P])
   0.5*(y1[end][1] + y2[end][2])
 end
+
+## Routines for Jacobi matrices (OPRL)
 
 function permat(pot::Array, h)
   n = 2*length(pot)
@@ -25,6 +31,7 @@ function permat(pot::Array, h)
   return C
 end
 
+# A single recursion matrix for OPRL/Jacobi
 function rmatRL(a::Number, x::Number)
   A = Array(Float64, 2, 2)
   A[1,1] = x - a
@@ -34,6 +41,7 @@ function rmatRL(a::Number, x::Number)
   A
 end
 
+# Transfer matrix for OPRL/Jacobi
 function tmatRL(a::Array, x::Number)
   A = eye(2)
   for i = 1:length(a)
@@ -42,6 +50,7 @@ function tmatRL(a::Array, x::Number)
   A
 end
 
+# Floquet discriminant for OPRL/Jacobi 
 function discRL(pot::Array, x::Number)
   n = length(pot)
   A = SymTridiagonal(pot, ones(typeof(pot[1]),n-1)) - SymTridiagonal(fill(x, n), zeros(n-1))
@@ -49,7 +58,7 @@ function discRL(pot::Array, x::Number)
   return (det(A) - det(B))
 end
 
-#Incomplete, fix this
+# Incomplete, fix this
 function rmatGC(α::Number, z::Number, j::Int64)
   A = Array(typeof(α), 2, 2)
   A[1,1] = z
@@ -60,6 +69,9 @@ function rmatGC(α::Number, z::Number, j::Int64)
   A
 end
 
+## Routines for Szego recursion (OPUC)
+
+# A recurrence matrix
 function rmatUC(α::Number, z::Number)
   A = Array(typeof(α), 2, 2)
   A[1,1] = z
@@ -70,6 +82,7 @@ function rmatUC(α::Number, z::Number)
   A
 end
 
+# A transfer matrix for Szego recursion a la Simon
 function tmatUC(α::Array, z::Number)
   n = length(α)
   A = eye(2)
@@ -79,6 +92,7 @@ function tmatUC(α::Array, z::Number)
   A
 end
 
+# The transfer matrix for the Geronimo-Case version of OPUC recursion
 function tmatGC(α::Array, z::Number)
   A = tmatUC(α, z)
   A *= diagm([1, z^(-n)])
@@ -87,7 +101,7 @@ end
 function discUC(α::Array, z::Number)
   n = length(α)
   if isodd(n)
-    error("For technical reasons we assume even period")
+    error("For annoying technical reasons we assume even period. Try a vector of even length")
   end
   return z^(-n/2) * trace(tmatUC(α, z))
 end
