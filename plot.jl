b59323e7df1be66eaa0dcb5113134d8f8294fdb7
Dylan Murphy <dtaquinas@gmail.com>
2016-11-07 17:13:08 -0700

    trying to fix adaptive map

--- plot.jl
+++ plot.jl
@@ -1,3 +1,5 @@
+module PlotFloq
+
 function findbandUC(disc::Function, domain)
   counter = 1
   if abs(disc(domain[1])) < 2 
@@ -56,19 +58,22 @@ function spikeUC(disc::Function, domain)
   y
 end
 
-
-
-function adspikeUC(α::Array, tol::Float64)
+function adspikeUC(disc::Function, tol::Float64)
   if tol <= 0
     error("Tolerance must be positive")
   end
   currentx = 0
-  dx = 1/tol
+  dx = tol
+  mintol = tol
   y = 0.
-  disc(x) = real(Floquet.discUC(α, exp(im * x)))
+  #disc(x) = real(Floquet.discUC(α, exp(im * x)))
   function nextstep()
     dy = abs(disc(currentx + dx) - disc(currentx))
     dx *= tol/dy
+    if dx < mintol
+      mintol = dx
+    end
+    #print(dx)
     #y = disc(currentx + dx)
     #currentx += dx
   end
@@ -77,6 +82,7 @@ function adspikeUC(α::Array, tol::Float64)
       nextstep()
       y = disc(currentx + dx)
       currentx += dx
+      #print(currentx, "\n")
       if abs(y) > 2
         break
       end
@@ -85,30 +91,81 @@ function adspikeUC(α::Array, tol::Float64)
   while true                         #The next part is to locate the lower band edge. If we didn't start in a band we can begin here
       nextstep()
       y = disc(currentx + dx)
+      currentx += dx
+      #print(currentx, "\n")
       if abs(y) < 2
         break
       end
-      currentx += dx
   end
+  print(currentx)
                                      #Now we compute the spike map
-
-
+  start = currentx
+  stepest = round(2π / mintol) 
+  output1 = zeros(Complex{Float64}, Int64(round(stepest*(1 - currentx / (2π)))))
+  output0 = zeros(Complex{Float64}, Int64(round(stepest*currentx / (2π))))
+  y = disc(currentx)
+  y2 = disc(currentx + dx) 
+  dy = y2 - y
+  i = 1
+  while currentx < 2π
+    sq = sqrt(4. + 0.0im - y^2)
+    if i < length(output1)
+      output1[i+1] = output1[i] + sign(dy) * real(dy / sq) - im * sign(y) * imag(dy / sq)
+      i += 1
+    else
+      nextout = output1[i] + sign(dy) * real(dy / sq) - im * sign(y) * imag(dy / sq)
+      output1 = vcat(output1, nextout)
+      i += 1
+    end
+    nextstep()
+    y = y2
+    currentx += dx
+    y2 = disc(currentx)
+  end
+  if i <= length(output1)
+    resize!(output1, i-1)
+  end
+  currentx = 0
+  i = 1
+#  sq = sqrt(4. + 0.0im - y^2)
+#  output0[1] = output1[end] + sign(dy) * real(dy / sq) - im * sign(y) * imag(dy / sq)
+  while currentx < start
+      sq = sqrt(4. + 0.0im - y^2)
+    if i < length(output0)
+      output0[i+1] = output0[i] + sign(dy) * real(dy / sq) - im * sign(y) * imag(dy / sq)
+      i += 1
+    else
+      nextout = output0[i] + sign(dy) * real(dy / sq) - im * sign(y) * imag(dy / sq)
+      output0 = vcat(output0, nextout)
+      i += 1
+    end
+    nextstep()
+    y = y2
+    currentx += dx
+    y2 = disc(currentx)
+  end
+  if i <= length(output0)
+    resize!(output0, i-1)
+  end
+  vcat(output0, output1)
 end
 
 
   
-function spike(disc::Function, domain, p::Number)
+#function spike(disc::Function, domain, p::Number)
   #δ = domain[2] - domain[1]
-  y = map(disc, domain) + 0.*im
+#  y = map(disc, domain) + 0.*im
   #dy = diff(y) / δ
-  dy = diff(y)
-  spre = zeros(dy)
-  spim = zeros(dy)
-  spsq = sqrt(1 - y .^ 2)
-  for i = 1:(length(dy)-1)
-    spre[i+1] = spre[i] + abs(real(dy[i] / spsq[i]))
-    spim[i+1] = spim[i] + imag(dy[i] / spsq[i])
-  end
-  return (1/p)*spre, spim
+#  dy = diff(y)
+#  spre = zeros(dy)
+#  spim = zeros(dy)
+#  spsq = sqrt(1 - y .^ 2)
+#  for i = 1:(length(dy)-1)
+#    spre[i+1] = spre[i] + abs(real(dy[i] / spsq[i]))
+#    spim[i+1] = spim[i] + imag(dy[i] / spsq[i])
+#  end
+#  return (1/p)*spre, spim
+#end
+
 end
 
